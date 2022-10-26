85631358694c66a53b9486f487e07508877ced91
kunalkatara12 <kunalkatara888@gmail.com>
2022-10-26 14:23:00 +0530

    new changes 26-10-22 14:22

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Footer, Main, Navbar } from "./components";
-
+import "./App.css"
 const App = () => {
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./style.css";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        <ul className="app__navbar-links">
+          {["Home", "About", "Projects",  "Contact"].map((item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
