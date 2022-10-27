e2b7f22b65bf8745c93505dfb60488977c05b9ee
kunalkatara12 <kunalkatara888@gmail.com>
2022-10-27 15:45:08 +0530

    changes in skill section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React from "react";
+import "./Skills.scss";
+import {
+  DiCss3,
+  DiBootstrap,
+  DiHtml5,
+  DiJava,
+  DiGit,
+  DiGithubBadge,
+  DiJavascript,
+  DiMongodb,
+  DiNodejs,
+  DiNpm,
+  DiPython,
+  DiSass,
+  DiSqllite,
+  DiReact,
+} from "react-icons/di";
+import { SiTailwindcss, SiCplusplus,  SiExpress } from "react-icons/si";
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <>
+      <div id="skills">
+        <section id="skills__section">
+          <h3 id="title">Skills</h3>
+          <div id="skills__circular-progress"></div>
+          <div id="skills__tools-techno">
+            <DiBootstrap className="tech__icons" />
+            <DiCss3 className="tech__icons" />
+            <DiHtml5 className="tech__icons" />
+            <DiJava className="tech__icons" />
+            <DiGit className="tech__icons" />
+            <DiGithubBadge className="tech__icons" />
+            <DiJavascript className="tech__icons" />
+            <DiMongodb className="tech__icons" />
+            <DiNodejs className="tech__icons" />
+            <DiNpm className="tech__icons" />
+            <DiPython className="tech__icons" />
+            <DiSass className="tech__icons" />
+            <DiSqllite className="tech__icons" />
+            <DiReact className="tech__icons" />
+            <SiTailwindcss className="tech__icons" />
+            <SiCplusplus className="tech__icons" />
+            <SiExpress className="tech__icons" />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default Skills
+export default Skills;
