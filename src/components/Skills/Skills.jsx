c879531c00359a95647a4f90d54bdc74e8884a5f
kunalkatara12 <kunalkatara888@gmail.com>
2022-10-28 08:03:45 +0530

    28-10-22 08:03

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -17,88 +17,133 @@ import {
   DiReact,
 } from "react-icons/di";
 import { SiTailwindcss, SiCplusplus, SiExpress } from "react-icons/si";
-import { BiCodeCurly } from "react-icons/bi";
+import { BiCodeCurly, BiServer } from "react-icons/bi";
 import { RiArrowDropDownLine } from "react-icons/ri";
 const Skills = () => {
   return (
-      <div id="skills">
-        <section id="skills__section">
-          <h3 id="title">Skills</h3>
-          <span id="subtitle">My technical level</span>
-          <div id="skills__container container grid">
-            {" "}
-            <div id="skills__content-progress">
-              <div id="skills__header">
-                <BiCodeCurly />
-                <div id="">
-                  <h1 id="skills__title">Frontend Developer</h1>
-                  <span id="skills__subttitle">More than 2 years</span>
+    <div id="skills">
+      <section id="skills__section">
+        <h3 id="skills__title">Skills</h3>
+        <h5 id="skills__subtitle">My technical level</h5>
+        <div id="skills__container container grid">
+          {" "}
+          <div>
+          {/* Skills 1 */}
+          <div className="skills__content-progress">
+            <div className="skills__header">
+              <BiCodeCurly />
+              <div className="">
+                <h1 className="skills__title">Frontend Developement</h1>
+                <span className="skills__subttitle">More than 2 years</span>
+              </div>
+              <RiArrowDropDownLine />
+              <div className="skills__list grid">
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">HTML</h3>
+                    <span className="skill__progress-percent">95%</span>
+                  </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__html"></span>
+                  </div>
                 </div>
-                <RiArrowDropDownLine />
-                <div id="skills__list grid">
-                  <div id="skills__data">
-                    <div id="skills__title">
-                      <h3 id="skill__name">HTML</h3>
-                      <span id="skill__progress-percent">95%</span>
-                    </div>
-                    <div id="skills__progress-bar">
-                      <span id="skill__percentage skill__html"></span>
-                    </div>
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">CSS</h3>
+                    <span className="skill__progress-percent">85%</span>
                   </div>
-                  <div id="skills__data">
-                    <div id="skills__title">
-                      <h3 id="skill__name">CSS</h3>
-                      <span id="skill__progress-percent">85%</span>
-                    </div>
-                    <div id="skills__progress-bar">
-                      <span id="skill__percentage skill__css"></span>
-                    </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__css"></span>
                   </div>
-                  <div id="skills__data">
-                    <div id="skills__title">
-                      <h3 id="skill__name">JavaScript</h3>
-                      <span id="skill__progress-percent">75%</span>
-                    </div>
-                    <div id="skills__progress-bar">
-                      <span id="skill__percentage skill__javascript"></span>
-                    </div>
+                </div>
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">JavaScript</h3>
+                    <span className="skill__progress-percent">75%</span>
                   </div>
-                  <div id="skills__data">
-                    <div id="skills__title">
-                      <h3 id="skill__name">React</h3>
-                      <span id="skill__progress-percent">80%</span>
-                    </div>
-                    <div id="skills__progress-bar">
-                      <span id="skill__percentage skill__react"></span>
-                    </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__javascript"></span>
+                  </div>
+                </div>
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">React</h3>
+                    <span className="skill__progress-percent">80%</span>
+                  </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__react"></span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* Skills 1 */}
+          {/* Skills 2 */}
+          <div className="skills__content-progress">
+            <div className="skills__header">
+              <BiServer />
+              <div className="">
+                <h1 className="skills__title">Backend Developenent</h1>
+                <span className="skills__subttitle">6 months</span>
+              </div>
+              <RiArrowDropDownLine />
+              <div className="skills__list grid">
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">Node.Js</h3>
+                    <span className="skill__progress-percent">55%</span>
+                  </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__node"></span>
+                  </div>
+                </div>
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">Express.Js</h3>
+                    <span className="skill__progress-percent">45%</span>
+                  </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__express"></span>
+                  </div>
+                </div>
+                <div className="skill__data">
+                  <div className="skill__title">
+                    <h3 className="skill__name">MongoDB</h3>
+                    <span className="skill__progress-percent">55%</span>
+                  </div>
+                  <div className="skill__progress-bar">
+                    <span className="skill__percentage skill__mongo"></span>
+                  </div>
+                </div>
 
-          <div id="skills__tools-techno">
-            <DiBootstrap className="tech__icons" />
-            <DiCss3 className="tech__icons" />
-            <DiHtml5 className="tech__icons" />
-            <DiJava className="tech__icons" />
-            <DiGit className="tech__icons" />
-            <DiGithubBadge className="tech__icons" />
-            <DiJavascript className="tech__icons" />
-            <DiMongodb className="tech__icons" />
-            <DiNodejs className="tech__icons" />
-            <DiNpm className="tech__icons" />
-            <DiPython className="tech__icons" />
-            <DiSass className="tech__icons" />
-            <DiSqllite className="tech__icons" />
-            <DiReact className="tech__icons" />
-            <SiTailwindcss className="tech__icons" />
-            <SiCplusplus className="tech__icons" />
-            <SiExpress className="tech__icons" />
+              </div>
+            </div>
           </div>
-        </section>
-      </div>
- 
+          {/* Skills 2 */}
+        </div>
+        </div>
+
+        <div id="skills__tools-techno">
+          <DiBootstrap className="tech__icons" />
+          <DiCss3 className="tech__icons" />
+          <DiHtml5 className="tech__icons" />
+          <DiJava className="tech__icons" />
+          <DiGit className="tech__icons" />
+          <DiGithubBadge className="tech__icons" />
+          <DiJavascript className="tech__icons" />
+          <DiMongodb className="tech__icons" />
+          <DiNodejs className="tech__icons" />
+          <DiNpm className="tech__icons" />
+          <DiPython className="tech__icons" />
+          <DiSass className="tech__icons" />
+          <DiSqllite className="tech__icons" />
+          <DiReact className="tech__icons" />
+          <SiTailwindcss className="tech__icons" />
+          <SiCplusplus className="tech__icons" />
+          <SiExpress className="tech__icons" />
+        </div>
+      </section>
+    </div>
   );
 };
 
