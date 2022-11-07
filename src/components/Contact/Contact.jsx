828496bd9a9b010da35d3b6b76bb9bdadc74da17
kunalkatara12 <kunalkatara888@gmail.com>
2022-11-07 15:20:07 +0530

    07-11-22 15:20

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Contact.scss";
+import { BiPhoneCall } from "react-icons/bi";
+import { FiMail } from "react-icons/fi";
+import { GoLocation } from "react-icons/go";
 const Contact = () => {
   return (
     <div id="contact">
@@ -43,13 +46,34 @@ const Contact = () => {
                 <div className="row100">
                   <div className="inptBox">
                     <span>Submit</span>
-                     <input type="submit" value="Send" id="submitBtn"/>
+                    <input type="submit" value="Send" id="submitBtn" />
                   </div>
                 </div>
               </div>
             </form>
           </div>
-          <div className="contact__box-inside info"></div>
+          <div className="contact__box-inside info">
+            <h3 id="info__title">Contact Info</h3>
+            <div className="info__box">
+              <div className="info__box-items">
+                <GoLocation className="contact__icons" />
+                <p>
+                  Bharatpur,Rajasthan <br />
+                  India
+                </p>
+              </div>
+              <div className="info__box-items">
+                <FiMail className="contact__icons" />
+                <a href="http://mailto:kunalkatara888@gmail.com">
+                  kunalkatara888@gmail.com
+                </a>
+              </div>
+              <div className="info__box-items">
+                <BiPhoneCall className="contact__icons" />
+                <a href="tel:+918955516491">+918955516491</a>
+              </div>
+            </div>
+          </div>
           <div className="contact__box-inside map">map</div>
         </div>
       </div>
