f6f51d2c73d121b91d7ee782398380226383fcb2
kunalkatara12 <kunalkatara888@gmail.com>
2023-01-11 00:27:30 +0530

    changes in Project

---
+++ src/components/Projects/Project/Project.jsx
@@ -0,0 +1,83 @@
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import { Button, CardActionArea, CardActions } from "@mui/material";
+// function Project() {
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardActionArea>
+//         <CardMedia
+//           component="img"
+//           height="140"
+//           image="/static/images/cards/contemplative-reptile.jpg"
+//           alt="green iguana"
+//         />
+//         <CardContent>
+//           <Typography gutterBottom variant="h5" component="div">
+//             Lizard
+//           </Typography>
+//           <Typography variant="body2" color="text.secondary">
+//             Lizards are a widespread group of squamate reptiles, with over 6,000
+//             species, ranging across all continents except Antarctica
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//       <CardActions>
+//         <Button size="small" color="primary">
+//           Share
+//         </Button>
+//       </CardActions>
+//     </Card>
+//   );
+
+// }
+
+// export default Project
+
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+ function Project(props) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        maxHeight: 400,
+        borderRadius: "10px",
+        backgroundColor: "#020936",
+        color: "white",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia component="img" height="140" image="" alt="green iguana" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+           {props.name}
+          </Typography>
+          <Typography variant="body2" color="white">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+            cumque maiores odio quaerat minus temporibus debitis optio ipsum in
+            dicta tenetur sed unde voluptatum quia eveniet nihil.
+          </Typography>{" "}
+          <CardActions sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Button size="small" color="primary">
+              Project Link
+            </Button>
+
+            <Button size="small" color="primary">
+              Github
+            </Button>
+          </CardActions>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default Project;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-
+import Project from "./Project/Project";
+import "./Projects.scss"
 const Projects = () => {
   return (
     <div id="projects">
       <section id="projects__section">
-        <h2>My Recent Work</h2>
-        <div className="container projects__container">
-          <article className="projects__item">
+        <h2 id="projects__heading" >My Recent Work</h2>
+        <div className="projects__container">
+          {/* <article className="projects__item">
             <div className="projects__item-image"></div>
             <h3>Title of Project</h3>
             <a href="https://github.com">Github</a>
-          </article>
+          </article> */}
+          <Project name="Sorting Visualizer" />
+          <Project name="Youtube Clone" />
+          <Project name="Zip Extractor" />
+          
         </div>
+        <div style={{fontWeight:"bold", marginTop:20}} >More Projects coming soon....</div>
       </section>
     </div>
   );
