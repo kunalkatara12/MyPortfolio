a6cb1bd6522d6d033daca2f1ba7322606bf23a4b
kunalkatara12 <kunalkatara888@gmail.com>
2023-01-11 19:09:33 +0530

    changes in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,25 +16,25 @@ const Footer = () => {
         {" "}
         <h4>Let's connect</h4>
         <div className="icon_of_my-handles">
-          <a href="https://www.instagram.com/kunalkatara2877/">
+          <a target={"blank"} href="https://www.instagram.com/kunalkatara2877/">
             <BsInstagram />
           </a>
-          <a href="">
+          <a target={"blank"} href="">
             <BsGithub />
           </a>
-          <a href="https://www.linkedin.com/in/kunal-k-9809391b3/">
+          <a target={"blank"} href="https://www.linkedin.com/in/kunal-k-9809391b3/">
             <BsLinkedin />
           </a>{" "}
-          <a href="">
+          <a target={"blank"} href="">
             <BsTwitter />
           </a>{" "}
-          <a href="">
+          <a target={"blank"} href="">
             <BsMedium />
           </a>
           <a href="">
             <SiCodechef />
           </a>
-          <a href="">
+          <a target={"blank"} href="">
             <SiHackerrank />
           </a>
         </div>
