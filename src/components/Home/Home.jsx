c8a6f2ed63e9be8dd32a09bab6bf2ee7cf4fe068
kunalkatara12 <kunalkatara888@gmail.com>
2023-02-15 17:23:14 +0530

    added download resume option

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Me from "../../assets/1586342076871.jpeg";
-import "./Home.scss"
+import Resume from "../../assets/Kunal's Resume.pdf";
+import Button from "@mui/material/Button";
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
+import "./Home.scss";
 const Home = () => {
+  const theme = createTheme({
+    palette: {
+      success: {
+        main: "#03e9f4",
+      },
+    },
+  });
   return (
     <>
       <div id="home">
         <div id="container">
           <div id="home_col_1">
-           
             <h2 id="description"></h2>
+            {""}
+            <ThemeProvider theme={theme}>
+              {" "}
+              <Button
+                style={{ marginTop: "50px" }}
+                href={Resume}
+                download
+                target="_blank"
+                variant="outlined"
+                color="success"
+                startIcon={<DownloadRoundedIcon />}
+              >
+                Download Resume
+              </Button>
+            </ThemeProvider>
+
+            {/* <a href={Resume}  >Download my resume</a> */}
           </div>
-          <div id="home_col_2"><img src={Me} alt="img" /></div>
+          <div id="home_col_2">
+            <img src={Me} alt="img" />
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
