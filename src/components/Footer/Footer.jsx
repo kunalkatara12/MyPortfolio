844d676d79740c9fe357e91f124bc1feac78f20c
kunalkatara12 <kunalkatara888@gmail.com>
2023-02-15 16:47:00 +0530

    changes on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-icons/bs";
 import { SiCodechef, SiHackerrank } from "react-icons/si";
 const Footer = () => {
+  const style = { color: "white", fontSize: "1.2em" };
   return (
     <div id="footer">
       <h2 id="footer__title">Kunal Katara's Portfolio</h2>
@@ -17,25 +18,31 @@ const Footer = () => {
         <h4>Let's connect</h4>
         <div className="icon_of_my-handles">
           <a target={"blank"} href="https://www.instagram.com/kunalkatara2877/">
-            <BsInstagram />
+            <BsInstagram style={style}  />
           </a>
-          <a target={"blank"} href="">
-            <BsGithub />
+          <a target={"blank"} href="https://github.com/kunalkatara12">
+            <BsGithub style={style} />
           </a>
-          <a target={"blank"} href="https://www.linkedin.com/in/kunal-k-9809391b3/">
-            <BsLinkedin />
+          <a
+            target={"blank"}
+            href="https://www.linkedin.com/in/kunal-k-9809391b3/"
+          >
+            <BsLinkedin style={style} />
           </a>{" "}
-          <a target={"blank"} href="">
-            <BsTwitter />
+          <a target={"blank"} href="https://twitter.com/KunalKatara4">
+            <BsTwitter style={style} />
           </a>{" "}
-          <a target={"blank"} href="">
-            <BsMedium />
+          <a target={"blank"} href="https://medium.com/@kunalkatara888">
+            <BsMedium style={style} />
           </a>
-          <a href="">
-            <SiCodechef />
+          <a
+            target={"blank"}
+            href="https://www.codechef.com/users/kunalkatara12"
+          >
+            <SiCodechef style={style} />
           </a>
-          <a target={"blank"} href="">
-            <SiHackerrank />
+          <a target={"blank"} href="https://www.hackerrank.com/kunalkatara888">
+            <SiHackerrank style={style} />
           </a>
         </div>
       </div>
