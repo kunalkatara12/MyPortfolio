a4f37726fabc9dd8907f9c2071e33e71c1606d8c
kunalkatara12 <kunalkatara888@gmail.com>
2023-04-29 17:03:40 +0530

    changes on main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Education from "../Education/Education";
-import Experience from "../Experience/Experience";
+// import Experience from "../Experience/Experience";
 import Home from "../Home/Home";
 import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
