b50fba6192e65099ff5b6442ae61e73144f161b3
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-06 23:09:53 +0530

    changes in About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,6 +13,14 @@ const About = () => {
             <h5>Get To know</h5>
             <h2 id="heading">About Me</h2>
           </header>
+          <p style={{color:"black"}}>
+            Hi, My name is Kunal Katara, I'm currently a 4th-year B.Tech.
+            student majoring in Computer Science and Engineering at Shri Mata
+            Vaishno Devi University. I have a strong passion for web development
+            and an eagerness to apply my technical skills and knowledge to solve
+            real-world problems. I have made many projects using Reactjs and
+            Nodejs. I also contributed to an Open Source Program in 2022.
+          </p>
 
           <div className="container about__container">
             <div id="about__me">
